5c8bd8a6f3d95c121592b31d8e8355393ddc4311
Patrick Ehrhardt <patricke@posteo.de>
2025-03-08 12:30:35 -0600

    Finished player movement logic and rendering

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	game_continue = True
+	clock = pygame.time.Clock()
+	dt = 0
+	updateable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updateable, drawable)
+	player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,8 +20,12 @@ def main():
 			if event.type == pygame.QUIT:
 				game_continue = False
 				return
-		screen.fill(pygame.Color(255,255,255))
+		screen.fill("black")
+		updateable.update(dt)
+		for object in drawable:
+			object.draw(screen)
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
